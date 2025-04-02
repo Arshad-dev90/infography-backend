e8083ef0d0d1108c74362b66df6e3c39cb4bfd72
Arshad-dev90 <hussainarshad19059@gmail.com>
2025-04-02 22:18:01 +0530

    modified puppeteer launch code

--- controllers/api-controller.js
+++ controllers/api-controller.js
@@ -2,7 +2,14 @@ const puppeteer = require('puppeteer');
 
 const generateScreenshot = async(req, res) => {
     try {
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({
+            headless: "new",
+            executablePath: '/opt/render/.cache/puppeteer/chrome/linux-134.0.6998.165/chrome',
+            args: [
+                "--no-sandbox",
+                "--disable-setuid-sandbox"
+            ]
+        });
         const page = await browser.newPage();
         await page.goto(process.env.URL, { waitUntil : 'networkidle2' });
         const screenshot = await page.screenshot( { fullPage : true } )
